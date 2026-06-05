d358aeb24b985bd3b8d8a9d593e5d5a2e125bc18
Naufal Aji Ferdiansyah <naufalaji05@gmail.com>
2026-06-05 17:49:03 +0700

    fix: hapus localhost tracking uri untuk github actions

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,47 +1,24 @@
 import pandas as pd
 import mlflow
 import mlflow.sklearn
-
-from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 
-# =========================
-# Load Dataset
-# =========================
-df = pd.read_csv("winequality_preprocessing.csv")
+def main():
+    
+    df = pd.read_csv('winequality_preprocessing.csv')
+    X = df.drop('quality', axis=1)
+    y = df['quality']
 
-target_column = "quality"  # ganti sesuai target
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-X = df.drop(columns=[target_column])
-y = df[target_column]
+    mlflow.sklearn.autolog()
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X,
-    y,
-    test_size=0.2,
-    random_state=42
-)
+    with mlflow.start_run() as run:
+        model = RandomForestClassifier(n_estimators=100, random_state=42)
+        model.fit(X_train, y_train)
+        
+        mlflow.sklearn.log_model(model, "model")
 
-# =========================
-# MLflow Local Tracking
-# =========================
-mlflow.set_tracking_uri("http://127.0.0.1:5000")
-mlflow.set_experiment("Wine Quality Basic")
-
-# Autolog
-mlflow.sklearn.autolog()
-
-# =========================
-# Training
-# =========================
-with mlflow.start_run():
-
-    model = RandomForestClassifier(
-        random_state=42
-    )
-
-    model.fit(X_train, y_train)
-
-    score = model.score(X_test, y_test)
-
-    print(f"Accuracy: {score:.4f}")
+if __name__ == "__main__":
+    main()
