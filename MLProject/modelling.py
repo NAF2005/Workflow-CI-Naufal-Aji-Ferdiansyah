95e8d208f09cc63870e2185a71e908a335d4ad34
Naufal Aji Ferdiansyah <naufalaji05@gmail.com>
2026-06-06 13:17:09 +0700

    fix: bersihkan cache git dan paksa tracking uri ke file mlruns

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -6,25 +6,25 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 
 def main():
-    # SOLUSI PASTI: Paksa MLflow mencatat ke folder lokal (bukan ke server/port 5000)
+    # MEMAKSA TRACKING KE LOCAL FILE FOLDER (AGAR GERBANG 127.0.0.1 MATI TOTAL)
     mlflow.set_tracking_uri("file:./mlruns")
     mlflow.set_experiment("Wine Quality Basic")
     
-    # Load dataset
+    # Membaca dataset
     df = pd.read_csv('winequality_preprocessing.csv')
     X = df.drop('quality', axis=1)
     y = df['quality']
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # Autolog untuk pencatatan otomatis
+    # Mengaktifkan Autolog otomatis
     mlflow.sklearn.autolog()
 
     with mlflow.start_run() as run:
         model = RandomForestClassifier(n_estimators=100, random_state=42)
         model.fit(X_train, y_train)
         
-        # Simpan model secara eksplisit ke dalam folder "model"
+        # Menyimpan model ke folder "model" untuk diekstrak oleh Docker
         mlflow.sklearn.log_model(model, "model")
 
 if __name__ == "__main__":
